697c0f3fa7298d06155f63e3e3a4e5859245de0c
niranjan2052 <34083712+niranjan2052@users.noreply.github.com>
2024-06-06 00:12:27 +0545

    project initialized and navigation bar completed with the responseive desgin

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import { Navbar } from "./components/Navbar";
+import "./layout.scss"
+function App() {
+  return (
+    <div className="layout">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
