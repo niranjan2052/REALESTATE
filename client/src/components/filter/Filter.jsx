f7d5149607237709fd00e8f5d057bf0bf178e0d8
niranjan2052 <34083712+niranjan2052@users.noreply.github.com>
2024-06-06 18:25:27 +0545

    listPage created with list of properties and map side by side

--- client/src/components/filter/Filter.jsx
+++ client/src/components/filter/Filter.jsx
@@ -1,7 +1,81 @@
 import "./filter.scss";
 
 const Filter = () => {
-  return <div>filter</div>;
+  const types = ["Buy", "Rent"];
+  const properties = ["Apartment", "House", "Condo", "Land"];
+
+  return (
+    <div className="filter">
+      <h1>
+        Search results for <b>London</b>
+      </h1>
+      <div className="top">
+        <div className="item">
+          <label htmlFor="city">Location</label>
+          <input
+            type="text"
+            id="city"
+            name="city"
+            placeholder="City Location"
+          />
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="item">
+          <label htmlFor="type">Type</label>
+          <select id="type" name="type">
+            <option>Any</option>
+            {types.map((val) => {
+              return (
+                <option key={val} value={val}>
+                  {val}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+
+        <div className="item">
+          <label htmlFor="property">Property</label>
+          <select id="property" name="property">
+            <option>Any</option>
+            {properties.map((val) => {
+              return (
+                <option key={val} value={val}>
+                  {val}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <div className="item">
+          <label htmlFor="minPrice">Min Price</label>
+          <input
+            type="number"
+            id="minPrice"
+            name="minPrice"
+            placeholder="Any"
+          />
+        </div>
+        <div className="item">
+          <label htmlFor="maxPrice">Max Price</label>
+          <input
+            type="number"
+            id="maxPrice"
+            name="maxPrice"
+            placeholder="Max Price"
+          />
+        </div>
+        <div className="item">
+          <label htmlFor="bedroom">Bedroom</label>
+          <input type="number" id="bedroom" name="bedroom" placeholder="Any" />
+        </div>
+        <button>
+          <img src="/search.png" alt="" />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Filter;
--- client/src/routes/listPage/ListPage.jsx
+++ client/src/routes/listPage/ListPage.jsx
@@ -1,6 +1,8 @@
 import "./listPage.scss";
 import { listData } from "../../lib/dummydata.js";
 import Filter from "../../components/filter/Filter.jsx";
+import Card from "../../components/card/Card.jsx";
+import Map from "../../components/map/Map.jsx";
 
 const ListPage = () => {
   const data = listData;
@@ -9,9 +11,14 @@ const ListPage = () => {
       <div className="listContainer">
         <div className="wrapper">
           <Filter />
+          {data.map((item) => {
+            return <Card key={item.id} item={item} />;
+          })}
         </div>
       </div>
-      <div className="mapContainer">Map</div>
+      <div className="mapContainer">
+        <Map items={data}/>
+      </div>
     </div>
   );
 };
