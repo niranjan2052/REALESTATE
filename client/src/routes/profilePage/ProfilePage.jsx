79ba543efdad8d5dabc0b2ca8c68d076588ed64a
niranjan2052 <34083712+niranjan2052@users.noreply.github.com>
2024-06-09 01:21:41 +0545

    profile page updated and profile update page created

--- client/src/routes/profilePage/ProfilePage.jsx
+++ client/src/routes/profilePage/ProfilePage.jsx
@@ -1,30 +1,55 @@
 import Chat from "../../components/chat/Chat";
 import List from "../../components/list/List";
+import { useDispatch, useSelector } from "react-redux";
 import "./profilePage.scss";
+import { removeStorage } from "../../lib";
+import http from "../../http";
+import { toast } from "react-toastify";
+import { Link, useNavigate } from "react-router-dom";
+import { clearUser } from "../../store";
+import { useEffect } from "react";
 
 const ProfilePage = () => {
+  const user = useSelector((state) => state.user.value);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    http
+      .post("/auth/logout")
+      .then(({ data }) => {
+        removeStorage("user");
+        dispatch(clearUser());
+        toast.success(data.message);
+        navigate("/");
+      })
+      .catch(() => {})
+      .finally(() => {});
+  };
+
   return (
     <div className="profilePage">
       <div className="details">
         <div className="wrapper">
           <div className="title">
             <h1>User Information</h1>
-            <button>Update Profile</button>
+            <Link to="/updateProfile">
+              <button>Update Profile</button>
+            </Link>
           </div>
           <div className="info">
             <span>
-              Avatar:{" "}
-              <img
-                src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                alt=""
-              />
+              Avatar: <img src={user.avatar || "/noAvatar.jpeg"} alt="" />
             </span>
             <span>
-              Username: <b>John Doe</b>
+              Username: <b>{user.username}</b>
             </span>
             <span>
-              E-mail: <b>john@gmail.com</b>
+              E-mail: <b>{user.email}</b>
             </span>
+            <button onClick={handleLogout} className="logout">
+              Logout
+            </button>
           </div>
           <div className="title">
             <h1>My List</h1>
---
+++ client/src/routes/profileUpdatePage/ProfileUpdatePage.jsx
@@ -0,0 +1,105 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { inStorage } from "../../lib/index.js";
+import { setUser } from "../../store/index.js";
+import http from "../../http/index.js";
+import "./profileUpdatePage.scss";
+import { toast } from "react-toastify";
+import UploadWidget from "../../components/uploadWidget/UploadWidget.jsx";
+import { useState } from "react";
+
+const ProfileUpdatePage = () => {
+  const user = useSelector((state) => state.user.value);
+  const [avatar, setAvatar] = useState(user.avatar);
+  const dispatch = useDispatch();
+  const formik = useFormik({
+    initialValues: {
+      username: user.username,
+      email: user.email,
+      password: "",
+    },
+    validationSchema: Yup.object({
+      username: Yup.string(),
+      email: Yup.string(),
+      password: Yup.string(),
+    }),
+    onSubmit: (values, { setSubmitting }) => {
+      const { username, email, password } = values;
+      let newValue = null;
+      if (password === "") {
+        newValue = { username, email, avatar };
+      } else {
+        newValue = { username, email, password, avatar };
+      }
+      http
+        .post(`/user/${user.id}`, newValue)
+        .then(({ data }) => {
+          inStorage("user", JSON.stringify(data), true);
+          dispatch(setUser(data));
+          toast.success("User Info Updated Successfully");
+        })
+        .catch(() => {})
+        .finally(() => {
+          setSubmitting(false);
+        });
+    },
+  });
+  return (
+    <div className="profileUpdatePage">
+      <div className="formContainer">
+        <form onSubmit={formik.handleSubmit}>
+          <h1>Update Profile</h1>
+          <div className="item">
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              className="username"
+              id="username"
+              value={formik.values.username}
+              onChange={formik.handleChange}
+            />
+          </div>
+          <div className="item">
+            <label htmlFor="email">Email</label>
+            <input
+              type="text"
+              className="email"
+              id="email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+            />
+          </div>
+          <div className="item">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              className="password"
+              id="password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+            />
+          </div>
+          <button type="submit" disabled={formik.isSubmitting}>
+            Update
+          </button>
+        </form>
+      </div>
+      <div className="sideContainer">
+        <img src={avatar || "./noAvatar.jpeg"} alt="" className="avatar" />
+        <UploadWidget
+          uwConfig={{
+            cloudName: "dpdku6o5e",
+            uploadPreset: "realestate",
+            multiple: false,
+            maxImageFileSize: 2000000,
+            folder: "avatars",
+          }}
+          setAvatar={setAvatar}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ProfileUpdatePage;
