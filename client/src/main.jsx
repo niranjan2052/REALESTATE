dba4a68be9735ab5747822871fd0e8c3d2f7dbfc
niranjan2052 <34083712+niranjan2052@users.noreply.github.com>
2024-06-08 11:18:21 +0545

    toast added with redux provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,25 @@ import { Provider } from "react-redux";
 import store from "./store/index.js";
 import App from "./App.jsx";
 import "./index.scss";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="colored"
+    />
   </React.StrictMode>
 );
