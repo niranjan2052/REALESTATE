bf43a9b2b32153787f607693a428bcef7292336e
niranjan2052 <34083712+niranjan2052@users.noreply.github.com>
2024-06-11 00:13:00 +0545

    chat page updated

--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -1,98 +1,67 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./chat.scss";
+import { useSelector } from "react-redux";
+import http from "../../http";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
 
-const Chat = () => {
-  const [chat, setChat] = useState(true);
+const Chat = ({ chats }) => {
+  const user = useSelector((state) => state.user.value);
+  const [chatInfo, setChatInfo] = useState(null);
+
+  const handleOpenChat = async (id, receiver) => {
+    try {
+      const res = await http.get(`/chats/${id}`);
+      setChatInfo({ ...res.data, receiver });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="chat">
       <div className="messages">
         <h1>Messages</h1>
-        <div className="message">
-          <img
-            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-          />
-          <span>John Doe</span>
-          <p>Lorem Ipsum dolar sit amet...</p>
-        </div>
-        <div className="message">
-          <img
-            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-          />
-          <span>John Doe</span>
-          <p>Lorem Ipsum dolar sit amet...</p>
-        </div>
-        <div className="message">
-          <img
-            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-          />
-          <span>John Doe</span>
-          <p>Lorem Ipsum dolar sit amet...</p>
-        </div>
-        <div className="message">
-          <img
-            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-          />
-          <span>John Doe</span>
-          <p>Lorem Ipsum dolar sit amet...</p>
-        </div>
+        {chats?.map((chat) => {
+          return (
+            <div
+              className="message"
+              key={chat.id}
+              style={{
+                backgroundColor: chat.seenBy.includes(user.id)
+                  ? "white"
+                  : "#fecd514e",
+              }}
+              onClick={() => handleOpenChat(chat.id, chat.receiver)}
+            >
+              <img src={chat.receiver.avatar || "/noAvatar.jpeg"} alt="" />
+              <span>{chat.receiver.username}</span>
+              <p>{chat.lastMessage}</p>
+            </div>
+          );
+        })}
       </div>
-      {chat && (
+      {chatInfo && (
         <div className="chatBox">
           <div className="top">
             <div className="user">
-              <img
-                src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                alt=""
-              />
-              John Doe
+              <img src={chatInfo.receiver.avatar || "/noAvatar.jpeg"} alt="" />
+              {chatInfo.receiver.username}
             </div>
-            <span className="close" onClick={()=>setChat(null)}>+</span>
+            <span className="close" onClick={() => setChatInfo(null)}>
+              +
+            </span>
           </div>
           <div className="center">
-            <div className="chatMessage">
-              <p>Lorem ipsum, dolor sit amet consectetur quidem.</p>
-              <span>1 hour ago</span>
-            </div>
-            <div className="chatMessage own">
-              <p>Lorem ipsum, dolor sit amet consectetur quidem.</p>
-              <span>1 hour ago</span>
-            </div>
-            <div className="chatMessage">
-              <p>Lorem ipsum, dolor sit amet consectetur quidem.</p>
-              <span>1 hour ago</span>
-            </div>
-            <div className="chatMessage own">
-              <p>Lorem ipsum, dolor sit amet consectetur quidem.</p>
-              <span>1 hour ago</span>
-            </div>
-            <div className="chatMessage">
-              <p>Lorem ipsum, dolor sit amet consectetur quidem.</p>
-              <span>1 hour ago</span>
-            </div>
-            <div className="chatMessage own">
-              <p>Lorem ipsum, dolor sit amet consectetur quidem.</p>
-              <span>1 hour ago</span>
-            </div>
-            <div className="chatMessage">
-              <p>Lorem ipsum, dolor sit amet consectetur quidem.</p>
-              <span>1 hour ago</span>
-            </div>
-            <div className="chatMessage own">
-              <p>Lorem ipsum, dolor sit amet consectetur quidem.</p>
-              <span>1 hour ago</span>
-            </div>
-            <div className="chatMessage">
-              <p>Lorem ipsum, dolor sit amet consectetur quidem.</p>
-              <span>1 hour ago</span>
-            </div>
-            <div className="chatMessage own">
-              <p>Lorem ipsum, dolor sit amet consectetur quidem.</p>
-              <span>1 hour ago</span>
-            </div>
+            {chatInfo.messages.map((message) => {
+              return (
+                <div className="chatMessage own" key={message.id}>
+                  <p>{message.text}</p>
+                  <span>{dayjs(message.createdAt).fromNow()}</span>
+                </div>
+              );
+            })}
           </div>
           <div className="bottom">
             <textarea></textarea>
