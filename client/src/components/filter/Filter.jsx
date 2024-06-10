828ca650dc8b46a7a85369cfa0b0f45a3b71c1c0
niranjan2052 <34083712+niranjan2052@users.noreply.github.com>
2024-06-11 00:16:15 +0545

    search operation added to searchbar and filter

--- client/src/components/filter/Filter.jsx
+++ client/src/components/filter/Filter.jsx
@@ -1,13 +1,36 @@
+import { useState } from "react";
 import "./filter.scss";
+import { useSearchParams } from "react-router-dom";
 
 const Filter = () => {
-  const types = ["Buy", "Rent"];
-  const properties = ["Apartment", "House", "Condo", "Land"];
+  const types = ["buy", "rent"];
+  const properties = ["apartment", "house", "condo", "land"];
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [query, setQuery] = useState({
+    type: searchParams.get("type") || "",
+    city: searchParams.get("city") || "",
+    property: searchParams.get("property") || "",
+    minPrice: searchParams.get("minPrice") || 0,
+    maxPrice: searchParams.get("maxPrice") || 10000000,
+    bedroom: searchParams.get("bedroom") || 1,
+  });
+
+  const handleChange = (e) => {
+    setQuery({
+      ...query,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleFilter = () => {
+    setSearchParams(query);
+  };
 
   return (
     <div className="filter">
       <h1>
-        Search results for <b>London</b>
+        Search results for <b>{query.city}</b>
       </h1>
       <div className="top">
         <div className="item">
@@ -17,13 +40,20 @@ const Filter = () => {
             id="city"
             name="city"
             placeholder="City Location"
+            onChange={handleChange}
+            defaultValue={query.city}
           />
         </div>
       </div>
       <div className="bottom">
         <div className="item">
           <label htmlFor="type">Type</label>
-          <select id="type" name="type">
+          <select
+            id="type"
+            name="type"
+            onChange={handleChange}
+            defaultValue={query.type}
+          >
             <option>Any</option>
             {types.map((val) => {
               return (
@@ -37,7 +67,12 @@ const Filter = () => {
 
         <div className="item">
           <label htmlFor="property">Property</label>
-          <select id="property" name="property">
+          <select
+            id="property"
+            name="property"
+            onChange={handleChange}
+            defaultValue={query.property}
+          >
             <option>Any</option>
             {properties.map((val) => {
               return (
@@ -55,6 +90,8 @@ const Filter = () => {
             id="minPrice"
             name="minPrice"
             placeholder="Any"
+            onChange={handleChange}
+            defaultValue={query.minPrice}
           />
         </div>
         <div className="item">
@@ -64,13 +101,22 @@ const Filter = () => {
             id="maxPrice"
             name="maxPrice"
             placeholder="Max Price"
+            onChange={handleChange}
+            defaultValue={query.maxPrice}
           />
         </div>
         <div className="item">
           <label htmlFor="bedroom">Bedroom</label>
-          <input type="number" id="bedroom" name="bedroom" placeholder="Any" />
+          <input
+            type="number"
+            id="bedroom"
+            name="bedroom"
+            placeholder="Any"
+            onChange={handleChange}
+            defaultValue={query.bedroom}
+          />
         </div>
-        <button>
+        <button onClick={handleFilter}>
           <img src="/search.png" alt="" />
         </button>
       </div>
--- client/src/components/searchBar/SearchBar.jsx
+++ client/src/components/searchBar/SearchBar.jsx
@@ -1,17 +1,22 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import "./searchBar.scss";
 
 const SearchBar = () => {
   const types = ["buy", "rent"];
   const [query, setQuery] = useState({
     type: "buy",
-    location: "",
+    city: "",
     minPrice: 0,
     maxPrice: 0,
   });
 
   const switchType = (val) => {
     setQuery((prev) => ({ ...prev, type: val }));
+  };
+
+  const handleChange = (e) => {
+    setQuery((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
 
   return (
@@ -30,24 +35,35 @@ const SearchBar = () => {
         })}
       </div>
       <form>
-        <input type="text" name="location" placeholder="City Location" />
+        <input
+          type="text"
+          name="city"
+          placeholder="City"
+          onChange={handleChange}
+        />
         <input
           type="number"
-          name="location"
+          name="minPrice"
           placeholder="Min Price"
           min={0}
           max={100000000}
+          onChange={handleChange}
         />
         <input
           type="number"
-          name="location"
+          name="maxPrice"
           placeholder="Max Price"
           min={0}
           max={100000000}
+          onChange={handleChange}
         />
-        <button>
-          <img src="/search.png" alt="seaerch" />
-        </button>
+        <Link
+          to={`/list?type=${query.type}&city=${query.city}&minPrice=${query.minPrice}&maxPrice=${query.maxPrice}`}
+        >
+          <button>
+            <img src="/search.png" alt="seaerch" />
+          </button>
+        </Link>
       </form>
     </div>
   );
