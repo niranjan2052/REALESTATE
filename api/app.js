a72d89fbc685e5cd5612381ab0800458e0e7f0a5
niranjan2052 <34083712+niranjan2052@users.noreply.github.com>
2024-06-12 20:26:23 +0545

    error resolved

--- api/app.js
+++ api/app.js
@@ -14,7 +14,7 @@ app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(urlencoded());
-app.use("/",(req,res)=>{
+app.get("/",(_req,res)=>{
   res.send("RealEstate Sever!!");
 })
 app.use("/api/posts", postRoutes);
